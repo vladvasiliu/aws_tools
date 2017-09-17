63f3d744849ad575c8570e16691a9639672f0e6c
Vlad Vasiliu <vladvasiliun@yahoo.fr>
2017-09-17 13:23:34 +0200

    Frontend: Instance details panel is now a table.

--- aws_tools/frontend/src/components/instance_detail.vue
+++ aws_tools/frontend/src/components/instance_detail.vue
@@ -4,7 +4,6 @@
             <tbody>
             <tr><td class="text-right">ID</td><td colspan="2">{{ instance.id }}</td></tr>
             <tr><td class="text-right">Region</td><td colspan="2">{{ instance.region_name }}</td></tr>
-            <tr><td class="text-right">Backup</td><td>{{ instance.backup_time }}</td><td><b-form-radio v-model="instance.backup" :options="[true, false]" stacked></b-form-radio></td></tr>
             </tbody>
         </table>
     </b-card>
