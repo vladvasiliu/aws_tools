6d73569ce62d5429edf34550c2091af1566820aa
Vlad Vasiliu <vladvasiliun@yahoo.fr>
2019-01-02 10:56:10 +0100

    Moved signed in verification from main.vue to App.vue.

--- aws_tools/frontend/src/components/App.vue
+++ aws_tools/frontend/src/components/App.vue
@@ -3,3 +3,17 @@
     <router-view />
   </div>
 </template>
+
+<script>
+import { mapGetters } from 'vuex'
+
+export default {
+  computed: { ...mapGetters(['isAuthenticated']) },
+
+  created () {
+    if (!this.isAuthenticated) {
+      this.$router.push({name: 'AccountLogin'})
+    }
+  }
+}
+</script>
--- aws_tools/frontend/src/components/main.vue
+++ aws_tools/frontend/src/components/main.vue
@@ -16,21 +16,11 @@
 </template>
 
 <script>
-import { mapGetters } from 'vuex'
 import AccountList from './account_list.vue'
 import InstanceList from './instance_list.vue'
 import NavBar from './NavBar'
 
 export default {
-  components: { AccountList, InstanceList, NavBar },
-  computed: {
-    ...mapGetters(['isAuthenticated'])
-  },
-
-  created () {
-    if (!this.isAuthenticated) {
-      this.$router.push({name: 'AccountLogin'})
-    }
-  }
+  components: { AccountList, InstanceList, NavBar }
 }
 </script>
