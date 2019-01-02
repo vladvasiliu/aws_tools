1b969bd7f6c7e6d38eca9a6465d8d4e00543509f
Vlad Vasiliu <vladvasiliun@yahoo.fr>
2019-01-02 14:48:04 +0100

    Removed top border from instance detail table.

--- aws_tools/frontend/src/components/instanceDetail.vue
+++ aws_tools/frontend/src/components/instanceDetail.vue
@@ -2,7 +2,7 @@
   <b-card header="Details">
     <table class="table">
       <tbody>
-        <tr><td class="text-right">ID</td><td colspan="2">{{ instance.id }}</td></tr>
+        <tr><td class="text-right border-top-0">ID</td><td colspan="2" class="border-top-0">{{ instance.id }}</td></tr>
         <tr><td class="text-right">Region</td><td colspan="2">{{ instance.region_name }}</td></tr>
         <tr><td class="text-right">Backup</td><td colspan="2">
           <b-form-checkbox
