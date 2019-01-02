111e52b3f4f39ae22322c86c7202ff966acc5cf9
Vlad Vasiliu <vladvasiliun@yahoo.fr>
2019-01-02 18:43:32 +0100

    Instance list is no longer separated as vue-fragment wasn't working well.

--- aws_tools/frontend/src/components/instance_list.vue
+++ aws_tools/frontend/src/components/instance_list.vue
@@ -2,23 +2,55 @@
   <b-card header="Instances">
     <b-list-group
       flush>
-      <InstanceListDetail
-        v-for="instance in instances_for_selected_account"
-        :instance="instance"
-        :key="instance.id"
-      />
+      <template
+        v-for="instance in instances_for_selected_account">
+        <b-list-group-item
+          v-b-toggle="instance.id"
+          :key="instance.id"
+          action
+          class="border-top-1 border-bottom-0 m-0 d-flex justify-content-between align-items-center instance-name">
+          {{ instance._name }}
+          <font-awesome-icon
+            :icon="collapseIcon"
+            class="instance-name-caret" />
+        </b-list-group-item>
+
+        <b-collapse
+          :id="instance.id"
+          :key="instance.id + 'detail'"
+          class="w-100 align-self-center mb-2">
+          <b-list-group-item
+            class="d-flex justify-content-left"
+            @click.stop>
+            <instanceDetail
+              :instance="instance"
+              class="m-1"/>
+            <volumeList
+              :instance="instance"
+              class="m-1"/>
+          </b-list-group-item>
+        </b-collapse>
+      </template>
     </b-list-group>
   </b-card>
 </template>
 
 <script>
 import { mapGetters } from 'vuex'
+import { faCaretDown } from '@fortawesome/free-solid-svg-icons'
 
-import InstanceListDetail from './instance-list-detail'
+import instanceDetail from './instanceDetail.vue'
+import volumeList from './volumeList'
 
 export default {
   components: {
-    InstanceListDetail
+    instanceDetail,
+    volumeList
+  },
+  data () {
+    return {
+      collapseIcon: faCaretDown
+    }
   },
   computed: {
     ...mapGetters([
@@ -33,3 +65,15 @@ export default {
   }
 }
 </script>
+
+<style>
+.instance-name:hover .instance-name-caret {
+  opacity: inherit;
+}
+.instance-name .instance-name-caret {
+  opacity: 0.3;
+}
+.instance-name.collapsed .instance-name-caret {
+  transform: rotate(90deg);
+}
+</style>
