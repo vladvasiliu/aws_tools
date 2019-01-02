66000371289ef76fac61c84c79474f99d64ebdc7
Vlad Vasiliu <vladvasiliun@yahoo.fr>
2019-01-02 10:15:22 +0100

    Frontend: Added connected user name to navbar.

--- aws_tools/frontend/src/components/NavBar.vue
+++ aws_tools/frontend/src/components/NavBar.vue
@@ -8,10 +8,8 @@
       id="nav_dropdown_collapse"
       is-nav>
       <b-navbar-nav class="ml-auto">
-        <!-- Navbar dropdowns -->
-        <b-nav-item-dropdown
-          :text="userName"
-          right>
+        <b-nav-item-dropdown right>
+          <span slot="button-content"><font-awesome-icon :icon="userIcon" /> {{ userName }}</span>
           <b-dropdown-item @click="logout">Logout</b-dropdown-item>
         </b-nav-item-dropdown>
       </b-navbar-nav>
@@ -21,9 +19,19 @@
 
 <script>
 import { mapGetters } from 'vuex'
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { faUser } from '@fortawesome/free-solid-svg-icons'
+
+library.add(faUser)
 
 export default {
   name: 'NavBar',
+
+  data () {
+    return {
+      userIcon: faUser
+    }
+  },
 
   computed: { ...mapGetters(['userName']) },
 
--- aws_tools/frontend/src/main.js
+++ aws_tools/frontend/src/main.js
@@ -3,6 +3,7 @@ import VueRouter from 'vue-router'
 import BootstrapVue from 'bootstrap-vue/dist/bootstrap-vue.esm'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
 import 'bootstrap/dist/css/bootstrap.css'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 
 import App from './components/App.vue'
 import store from './store'
@@ -10,6 +11,8 @@ import router from './router'
 
 Vue.use(BootstrapVue)
 Vue.use(VueRouter)
+
+Vue.component('font-awesome-icon', FontAwesomeIcon)
 
 // eslint-disable-next-line no-new
 new Vue({
