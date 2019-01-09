2579c1992042d64ce64bf743e980d0566d5c42da
Vlad Vasiliu <vladvasiliun@yahoo.fr>
2019-01-09 14:31:18 +0100

    Improved query to find volumes to snapshot.

--- aws_tools/managers.py
+++ aws_tools/managers.py
@@ -1,16 +1,19 @@
 from django.db import models
+from django.db.models import Q, Max
 from django.utils import timezone as tz
 
 
 class EBSVolumeManager(models.Manager):
     def to_snapshot(self):
         now = tz.now()
-        #volumes = self.filter(backup=True, instance__backup=True, instance__backup_time__lt=now.time())
-        volumes = self.filter(instance__backup=True, instance__backup_time__lt=now.time(), present=True)
-        to_snapshot = []
-        for vol in volumes:
-            if vol.ebssnapshot_set.count() == 0:
-                to_snapshot.append(vol)
-            elif vol.ebssnapshot_set.latest().created_at.date() < now.date():
-                to_snapshot.append(vol)
-        return to_snapshot
+        # volumes = self.filter(instance__backup=True, instance__backup_time__lt=now.time(), present=True)
+        # to_snapshot = []
+        # for vol in volumes:
+        #     if vol.ebssnapshot_set.count() == 0:
+        #         to_snapshot.append(vol)
+        #     elif vol.ebssnapshot_set.latest().created_at.date() < now.date():
+        #         to_snapshot.append(vol)
+        # return to_snapshot
+        return self.filter(instance__backup=True, instance__backup_time__lt=now.time(), present=True)\
+            .annotate(latest_snapshot_date=Max('ebssnapshot__created_at')) \
+            .filter(Q(latest_snapshot_date__lt=now.date()) | Q(latest_snapshot_date__isnull=True))
