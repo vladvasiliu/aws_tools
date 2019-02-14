fbbb1d4ad2e2a47c096c7fb0a07f0d847a60d928
Vlad Vasiliu <vladvasiliun@yahoo.fr>
2019-02-14 17:34:37 +0100

    Backend: Added AWSOrg to admin site.

--- aws_tools/admin.py
+++ aws_tools/admin.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
 
-from .models import AWSAccount, Instance, EBSVolume, EBSSnapshot
+from .models import AWSAccount, Instance, EBSVolume, EBSSnapshot, AWSOrganization
 
 # Register your models here.
 admin.site.register(AWSAccount)
 admin.site.register(Instance)
 admin.site.register(EBSVolume)
 admin.site.register(EBSSnapshot)
+admin.site.register(AWSOrganization)
