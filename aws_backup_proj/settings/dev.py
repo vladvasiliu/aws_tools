9622dd60983e524101299dfe584a3b6b308192de
Vlad Vasiliu <vladvasiliun@yahoo.fr>
2019-11-12 10:25:40 +0100

    Backend: Use AWS Secrets Manager for DB settings

--- aws_backup_proj/settings/dev.py
+++ aws_backup_proj/settings/dev.py
@@ -1,4 +1,5 @@
 from .base import *
+from .secrets import get_secret
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'vto4v-mncswzcm*fynqqp+@wsj9#smof=nh09xnzjrgae0h9sj'
@@ -9,21 +10,23 @@ DEBUG = True
 # Database
 # https://docs.djangoproject.com/en/1.10/ref/settings/#databases
 
+secret_region = 'eu-west-3'
+
+db_secret = get_secret('aws-tools/dev/rds', secret_region)
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'aws_tools',
-        'HOST': '127.0.0.1',
-        'USER': 'postgres',
-        'PASSWORD': 'mysecretpassword'
+        'NAME': 'aws-tools-dev',
+        'HOST': db_secret["host"],
+        'USER': db_secret["username"],
+        'PASSWORD': db_secret["password"],
     }
 }
 
 CELERY_BROKER_URL = "amqp://celery:celery@192.168.56.9:5672/celery"
 CORS_ORIGIN_ALLOW_ALL = True
 CORS_ORIGIN_WHITELIST = (
-    '127.0.0.1:8080',
-    'localhost:8080',
     'http://127.0.0.1:8080',
     'http://localhost:8080',
     'null',
