361292547382d90a4e7767308ab3ca433cb56d7f
Vlad Vasiliu <vladvasiliun@yahoo.fr>
2019-11-20 17:35:30 +0100

    Frontend: Added loading component to SecGroupList

--- aws_tools/frontend/src/components/SecurityGroupList.vue
+++ aws_tools/frontend/src/components/SecurityGroupList.vue
@@ -1,17 +1,32 @@
 <template>
-  <div>
+  <div v-if="!api_status.busy && !api_status.error">
     {{ securityGroupList }}
   </div>
+  <loading v-else />
 </template>
 
 <script>
 import { mapGetters } from 'vuex'
+import Loading from './loading'
 
 export default {
   name: 'SecurityGroupList',
 
+  components: {
+    Loading
+  },
+
   props: {
     selectedAccount: { type: Object, default: null }
+  },
+
+  data () {
+    return {
+      api_status: {
+        busy: true,
+        error: false
+      }
+    }
   },
 
   computed: {
@@ -20,6 +35,11 @@ export default {
 
   mounted () {
     this.$store.dispatch('getSecurityGroupList')
+      .then(() => { this.api_status.busy = false })
+      .catch(error => {
+        this.api_status.busy = false
+        this.api_status.error = error
+      })
   }
 }
 </script>
