aa991c9df34860a1d129a8a4ff460d072979485e
Vlad Vasiliu <code@vladvasiliu.com>
2020-07-08 18:38:53 +0200

    Frontend: Added footer with version.

--- aws_tools/frontend/src/components/App.vue
+++ aws_tools/frontend/src/components/App.vue
@@ -1,17 +1,28 @@
 <template>
   <div id="app">
-    <NavBar />
-    <loading v-if="loading" />
-    <error-view
-      v-else-if="error"
-      :error="error"
-    />
-    <div
-      v-else
-      class="container mt-3 mb-3"
-    >
-      <router-view />
+    <div class="main-content">
+      <NavBar />
+      <loading v-if="loading" />
+      <error-view
+        v-else-if="error"
+        :error="error"
+      />
+      <div
+        v-else
+        class="container mt-3 mb-3"
+      >
+        <router-view />
+      </div>
     </div>
+    <footer class="footer bg-light text-secondary text-center">
+      <b-container>
+        <b-row>
+          <b-col>
+            AWS Tools <small>v0.1.0</small>
+          </b-col>
+        </b-row>
+      </b-container>
+    </footer>
   </div>
 </template>
 
@@ -38,3 +49,17 @@ export default {
   }
 }
 </script>
+
+<style>
+  div.main-content {
+    min-height: 100vh;
+    overflow: auto;
+    padding-bottom: 3em;
+  }
+  footer {
+    bottom: 0;
+    height: 3em;
+    margin-top: -3em;
+    padding-top: 0.5em;
+  }
+</style>
