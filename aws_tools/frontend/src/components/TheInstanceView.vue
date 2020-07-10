82650a6df2a76d29401f80f1089e4d832f0784ac
Vlad Vasiliu <code@vladvasiliu.com>
2020-07-10 09:50:11 +0200

    Frontend: TheInstanceView shows error message when failed to load instances.

--- aws_tools/frontend/src/components/TheInstanceView.vue
+++ aws_tools/frontend/src/components/TheInstanceView.vue
@@ -1,5 +1,13 @@
 <template>
-  <div class="row">
+  <loading v-if="loading" />
+  <error-view
+    v-else-if="error"
+    :error="error"
+  />
+  <div
+    v-else
+    class="row"
+  >
     <div class="col-3">
       <account-list
         :route-dest-fun="routeDestFun"
@@ -14,21 +22,29 @@
 
 <script>
 import { RouteDest } from './ObjectListGroupCard'
+import Loading from './loading'
+import { Error } from './ErrorView'
 
 export default {
   components: {
+    ErrorView: () => (import('./ErrorView')),
+    Loading,
     AccountList: () => ({ component: import('./AWSAccountList') })
   },
   data: function () {
     return {
-      routeDestAll: new RouteDest('All', { name: 'InstanceView' })
+      routeDestAll: new RouteDest('All', { name: 'InstanceView' }),
+      error: undefined,
+      loading: true
     }
   },
   mounted () {
-    this.$store.dispatch('LOAD_INSTANCE_LIST').then(
-      () => {},
-      () => {}
-    )
+    this.$store.dispatch('LOAD_INSTANCE_LIST')
+      .then(
+        () => {},
+        (err) => { this.error = new Error('Failed to retrieve instances', err.message) }
+      )
+      .finally(() => { this.loading = false })
   },
   methods: {
     routeDestFun: (account) => ({ name: 'InstanceViewForAccount', params: { id: account.id } })
