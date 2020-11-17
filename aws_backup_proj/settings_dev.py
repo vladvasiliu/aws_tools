64705d3baa828161e73fa994ccc180ce3a9ea09f
Vlad Vasiliu <vladvasiliun@yahoo.fr>
2020-11-17 16:59:37 +0100

    Added structlog.

---
+++ aws_backup_proj/settings_dev.py
@@ -0,0 +1,14 @@
+from .settings import *
+
+DEBUG = True
+
+LOGGING["root"] = {
+    "handlers": ["plain_console", ],
+    "level": "INFO",
+}
+LOGGING["loggers"] = {
+    "django.server": {
+        "handlers": [],
+        "propagate": False,
+    },
+}
