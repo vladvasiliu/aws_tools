c56d8cfd67810322d5d16e401af1812c6ac0e7be
Vlad Vasiliu <vladvasiliun@yahoo.fr>
2021-02-09 11:32:52 +0100

    Get EC2 IP and add it to allowed_hosts.

--- aws_backup_proj/get_env.py
+++ aws_backup_proj/get_env.py
@@ -4,6 +4,7 @@ import os
 import boto3
 from botocore.exceptions import ClientError
 from django.core.exceptions import ImproperlyConfigured
+import requests
 
 
 def value_from_env(key: str) -> str:
@@ -16,49 +17,65 @@ def value_from_env(key: str) -> str:
 
 # This comes from AWS Secrets Manager sample code
 
+
 def get_secret(secret_name: str, region_name: str):
     # Create a Secrets Manager client
     session = boto3.session.Session()
-    client = session.client(
-        service_name='secretsmanager',
-        region_name=region_name
-    )
+    client = session.client(service_name="secretsmanager", region_name=region_name)
 
     # In this sample we only handle the specific exceptions for the 'GetSecretValue' API.
     # See https://docs.aws.amazon.com/secretsmanager/latest/apireference/API_GetSecretValue.html
     # We rethrow the exception by default.
 
     try:
-        get_secret_value_response = client.get_secret_value(
-            SecretId=secret_name
-        )
+        get_secret_value_response = client.get_secret_value(SecretId=secret_name)
     except ClientError as e:
-        if e.response['Error']['Code'] == 'DecryptionFailureException':
+        if e.response["Error"]["Code"] == "DecryptionFailureException":
             # Secrets Manager can't decrypt the protected secret text using the provided KMS key.
             # Deal with the exception here, and/or rethrow at your discretion.
             raise e
-        elif e.response['Error']['Code'] == 'InternalServiceErrorException':
+        elif e.response["Error"]["Code"] == "InternalServiceErrorException":
             # An error occurred on the server side.
             # Deal with the exception here, and/or rethrow at your discretion.
             raise e
-        elif e.response['Error']['Code'] == 'InvalidParameterException':
+        elif e.response["Error"]["Code"] == "InvalidParameterException":
             # You provided an invalid value for a parameter.
             # Deal with the exception here, and/or rethrow at your discretion.
             raise e
-        elif e.response['Error']['Code'] == 'InvalidRequestException':
+        elif e.response["Error"]["Code"] == "InvalidRequestException":
             # You provided a parameter value that is not valid for the current state of the resource.
             # Deal with the exception here, and/or rethrow at your discretion.
             raise e
-        elif e.response['Error']['Code'] == 'ResourceNotFoundException':
+        elif e.response["Error"]["Code"] == "ResourceNotFoundException":
             # We can't find the resource that you asked for.
             # Deal with the exception here, and/or rethrow at your discretion.
             raise e
-        elif e.response['Error']['Code'] == 'AccessDeniedException':
+        elif e.response["Error"]["Code"] == "AccessDeniedException":
             raise e
     else:
         # Decrypts secret using the associated KMS CMK.
         # Depending on whether the secret is a string or binary, one of these fields will be populated.
-        if 'SecretString' in get_secret_value_response:
-            return json.loads(get_secret_value_response['SecretString'])
+        if "SecretString" in get_secret_value_response:
+            return json.loads(get_secret_value_response["SecretString"])
 
     # Your code goes here.
+
+
+def get_ec2_ip() -> str:
+    """Get the machine's IP as used by the load balancer.
+    The result should be added to ALLOWED_HOSTS to prevent Django from returning HTTP500.
+    For example, in your `settings.py`:
+    .. code-block::
+        ALLOWED_HOSTS = [
+            get_ec2_ip(),
+            "app.example.com"
+        ]
+    For the time being, this only supports the Metadata Version 1.
+    :return: A string containing the instance's IPv4
+    """
+    endpoint = "http://169.254.169.254/latest/meta-data/local-ipv4"
+    try:
+        # Use a short timeout, the metadata endpoint should answer quickly
+        return requests.get(endpoint, timeout=2.0).text
+    except Exception as e:
+        raise ImproperlyConfigured("Failed to retrieve IPv4 from EC2 instance metadata.") from e
--- aws_backup_proj/settings.py
+++ aws_backup_proj/settings.py
@@ -1,6 +1,6 @@
 # These are the production settings.
 
-from .get_env import value_from_env, get_secret
+from .get_env import value_from_env, get_secret, get_ec2_ip
 from .base_settings import *
 
 secret_name = value_from_env("AWS_SECRET_NAME")
@@ -8,18 +8,18 @@ secret_region = value_from_env("AWS_SECRET_REGION")
 conf_secret = get_secret(secret_name, secret_region)
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = conf_secret['secretKey']
+SECRET_KEY = conf_secret["secretKey"]
 
 # Database
 # https://docs.djangoproject.com/en/1.10/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': conf_secret['dbName'],
-        'HOST': conf_secret["dbHost"],
-        'USER': conf_secret["dbUsername"],
-        'PASSWORD': conf_secret["dbPassword"],
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": conf_secret["dbName"],
+        "HOST": conf_secret["dbHost"],
+        "USER": conf_secret["dbUsername"],
+        "PASSWORD": conf_secret["dbPassword"],
     }
 }
 
@@ -28,29 +28,27 @@ DATABASES = {
 # See https://docs.djangoproject.com/en/1.10/howto/deployment/checklist/
 
 ALLOWED_HOSTS = conf_secret.get("allowedHosts", ["127.0.0.1", "localhost"])
+ALLOWED_HOSTS.append(get_ec2_ip())
 
 OIDC_AUTH = {
     # Specify OpenID Connect endpoint. Configuration will be
     # automatically done based on the discovery document found
     # at <endpoint>/.well-known/openid-configuration
-    'OIDC_ENDPOINT': conf_secret['oidcEndpoint'],
-
+    "OIDC_ENDPOINT": conf_secret["oidcEndpoint"],
     # Accepted audiences the ID Tokens can be issued to
-    'OIDC_AUDIENCES': (conf_secret['oidcClientId'],),
-
+    "OIDC_AUDIENCES": (conf_secret["oidcClientId"],),
     # (Optional) Function that resolves id_token into user.
     # This function receives a request and an id_token dict and expects to
     # return a User object. The default implementation tries to find the user
     # based on username (natural key) taken from the 'sub'-claim of the
     # id_token.
-    'OIDC_RESOLVE_USER_FUNCTION': 'aws_tools.helpers.get_user_by_id',
-
+    "OIDC_RESOLVE_USER_FUNCTION": "aws_tools.helpers.get_user_by_id",
     # (Optional) Number of seconds in the past valid tokens can be
     # issued (default 600)
-    'OIDC_LEEWAY': 3900,
+    "OIDC_LEEWAY": 3900,
 }
 
 
 secure_proxy_ssl_header = conf_secret.get("secureProxySSLHeader", None)
 if secure_proxy_ssl_header:
-    SECURE_PROXY_SSL_HEADER = (secure_proxy_ssl_header, 'https')
+    SECURE_PROXY_SSL_HEADER = (secure_proxy_ssl_header, "https")
